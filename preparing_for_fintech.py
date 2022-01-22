2240177e91c22c8eebe947b04c8d38b2f4e73da6
slavog <slavog@ro.ru>
2022-01-22 17:54:07 +0300

    Initial commit

---
+++ preparing_for_fintech.py
@@ -0,0 +1,10 @@
+from memory_profiler import memory_usage
+T = int(input())
+for t in range(T):
+    text = list(input())
+    text.sort()
+    if text[0] == text[1] and text[2] == text[3] and text[1] != text[2]:
+        print('Yes')
+    else:
+        print('No')
+print(memory_usage())
